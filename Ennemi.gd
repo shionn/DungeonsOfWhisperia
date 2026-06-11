8e2f42c2182a0f3d1ef426d6586530c30dcc7eb2
Kepka Ludovic <shionn@gmail.com>
2026-06-11 07:01:05 +0200

    les ennemi voie le joueur

--- Ennemi.gd
+++ Ennemi.gd
@@ -3,10 +3,15 @@ extends CharacterBody3D
 
 
 func _physics_process(delta: float) -> void:
-	var space_state = get_world_3d().direct_space_state
 	var start = global_position+Vector3.UP
 	var end = $"../Player".global_position+Vector3.UP
-	print(start, " ", end)
-	var query = PhysicsRayQueryParameters3D.create(start, end)
-	var result = space_state.intersect_ray(query)
-	print(result)
+	var direction = (end-start).normalized()
+	var orientation = self.basis * Vector3.BACK
+	if rad_to_deg(orientation.angle_to(direction)) <= 60 :
+		var query = PhysicsRayQueryParameters3D.create(start, end)
+		var result = get_world_3d().direct_space_state.intersect_ray(query)
+		if (result && result.collider == $"../Player"):
+			print(self, "see player", result)
+		else:
+			#print(self, "does not see player")
+			pass
