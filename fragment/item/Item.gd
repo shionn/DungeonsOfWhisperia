53793f96e49339d86dab81988bce93cc399d6f8b
Kepka Ludovic <shionn@gmail.com>
2026-06-22 22:01:11 +0200

    Je seche sur le sitems

--- fragment/item/Item.gd
+++ fragment/item/Item.gd
@@ -2,3 +2,12 @@ class_name Item
 extends Node
 
 @export var icon : Resource
+
+signal action()
+
+func execute() -> void:
+	action.emit()
+
+func _give_back_4_pv() -> void:
+	$"../../Player".pv = min($"../../Player".pv+4, $"../../Player".maxpv)
+	
