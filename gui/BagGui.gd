6dc6a901d64fdd144519a53643efc1855765c6fc
Kepka Ludovic <shionn@gmail.com>
2026-06-23 20:53:03 +0200

    On peu louter

--- gui/BagGui.gd
+++ gui/BagGui.gd
@@ -1,43 +1,10 @@
 class_name BagGui
 extends Control
 
-const BiereRousseDeEve = "BiereRousseDeEve"
-const BoisDeQualite = "BoisDeQualite"
-const BouteilleCapitain = "BouteilleCapitain"
-const CarteHungConnut = "CarteHungConnut"
-const Champignon = "Champignon"
-const CrystalTeleportationOasis = "CrystalTeleportationOasis"
-const FruitDefendu = "FruitDefendu"
-const HacheKazrog = "HacheKazrog"
-const Houblon = "Houblon"
-const ListInvocationVent = "ListInvocationVent"
-const LivreTransfo = "LivreTransfo"
-const Melon = "Melon"
-const Mojito = "Mojito"
-const MojitoSansGlace = "MojitoSansGlace"
-const Ossement = "Ossement"
-const OssementBarbeDrue = "OssementBarbeDrue"
-const PageHungConnutFrag1 = "PageHungConnutFrag1"
-const PageHungConnutFrag2 = "PageHungConnutFrag2"
-const PageHungConnutFrag3 = "PageHungConnutFrag3"
-const ParcheminBarbeDrue = "ParcheminBarbeDrue"
-const ParcheminBarbeDrueDecoder = "ParcheminBarbeDrueDecoder"
-const ParcheminHungConnut = "ParcheminHungConnut"
-const PasseBarque = "PasseBarque"
-const PasseBarqueTanpon = "PasseBarqueTanpon"
-const Pioche = "Pioche"
-const Plume = "Plume"
-const PotionDegrisement = "PotionDegrisement"
-const TresorBarbeDrue = "TresorBarbeDrue"
-const TresorBarbeDrueNet = "TresorBarbeDrueNet"
-const TresorBarbeDrueMaudit = "TresorBarbeDrueMaudit"
-const TresorHungConnut = "TresorHungConnut"
-const TresorHungConnutNet = "TresorHungConnutNet"
-const SelDeMontagne = "SelDeMontagne"
-
 @onready var _grid = $PanelContainer/MarginContainer/VBoxContainer/Container as Container
-@onready var _items = $"../Items" as Control
-#@onready var _gui = $".." as Gui
+@onready var _container = $PanelContainer/MarginContainer/VBoxContainer/Container as Container
+@onready var _bag = $"/root/World/Player/Bag" as Bag
+@onready var _items = $"/root/World/Items" as Items
 
 #var drag : Item = null
 
@@ -51,29 +18,19 @@ func _process(_delta: float) -> void:
 	#	drag.global_position = get_viewport().get_mouse_position() + Vector2(5,5)
 	pass
 
-func loot(item_name : String) -> void :
-	var item = _items.get_node(item_name)
-	if item and _items.is_ancestor_of(item) :
-		_items.remove_child(item)
-		_grid.add_child(item)
-		#_gui.append_to_console("Vous obtenez <"+item.tooltip_text+">")
-		on_item_change.emit()
-	else :
-		print("loot error " + item_name)
+func refresh() -> void:
+	for child in _container.get_children() : child.queue_free()
+	for item_name in _bag.items :
+		var item : Item  = _items.from(item_name)
+		if item : 
+			var but = TextureButton.new();
+			but.texture_normal =  item.icon
+			but.tooltip_text = item.name
+			but.pressed.connect(func(): print(item))
+			_container.add_child(but)
 
-func unloot(item_name : String) -> void :
-	var item = _grid.get_node(item_name)
-	if item :
-		_grid.remove_child(item)
-		item.global_position = Vector2(-100,-100)
-		_items.add_child(item)
-		on_item_change.emit()
 
-func contain(item_name : String) -> bool:
-	return _grid.get_node(item_name) != null
-	
-func empty() :
-	return _grid.get_child_count() == 0
+
 
 func _on_close_button_pressed() -> void:
 	hide()
@@ -86,11 +43,11 @@ func save_game() -> void :
 	file.store_line(json_string)
 
 func load_game() -> void : 
-	if FileAccess.file_exists("user://bag.save") :
-		for item in _grid.get_children() : unloot(item.name)
-		var file = FileAccess.open("user://bag.save", FileAccess.READ)
-		for item_name in JSON.parse_string(file.get_line()) :
-			var item = _items.get_node(item_name)
-			_items.remove_child(item)
-			_grid.add_child(item)
+	#if FileAccess.file_exists("user://bag.save") :
+		#for item in _grid.get_children() : unloot(item.name)
+		#var file = FileAccess.open("user://bag.save", FileAccess.READ)
+		#for item_name in JSON.parse_string(file.get_line()) :
+			#var item = _items.get_node(item_name)
+			#_items.remove_child(item)
+			#_grid.add_child(item)
 	on_item_change.emit()
--- gui/LootGui.gd
+++ gui/LootGui.gd
@@ -3,6 +3,8 @@ extends GridContainer
 
 @onready var _container = $PanelContainer/MarginContainer/VBoxContainer/Container as Container
 @onready var _items = $"/root/World/Items" as Items
+@onready var _bag = $"/root/World/Player/Bag" as Bag
+@onready var _bag_gui = $"../Bag" as BagGui
 
 func _ready() -> void:
 	self.visible = false
@@ -14,7 +16,13 @@ func loot(monster:Monster) -> void:
 		var but = TextureButton.new();
 		but.texture_normal =  item.icon
 		but.tooltip_text = item.name
-		but.pressed.connect(func(): print(item))
+		but.pressed.connect(func(): self._loot_item(item, but, monster))
 		_container.add_child(but)
 	self.visible = true
+	
+func _loot_item(item:Item, button:TextureButton, monster:Monster) -> void:
+	_bag.loot(item.item_name)
+	monster.loot_obj = Items.ItemName.None
+	button.queue_free()
+	_bag_gui.refresh()
 	
