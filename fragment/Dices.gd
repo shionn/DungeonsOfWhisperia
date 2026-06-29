515b7fc9db1b8bf2b034d4e5e7147e9e82d85da6
Kepka Ludovic <shionn@gmail.com>
2026-06-29 22:43:00 +0200

    correction dice

--- fragment/Dices.gd
+++ fragment/Dices.gd
@@ -10,6 +10,10 @@ static func d6(count: int, threshold:int) -> int :
 			result = result+1
 	return result
 
-static func d3() -> int :
-	return _random.randi_range(1,3)
+static func d3(count: int = 1, threshold:int = 1) -> int :
+	var result = 0
+	for i in range(0,count) :
+		if threshold == 1 : result = result + _random.randi_range(1,3)
+		elif _random.randi_range(1,3) >= threshold : result = result+1
+	return result
 	
