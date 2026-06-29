b9d47ad36b934c43e362a005b074134305d27b15
Kepka Ludovic <shionn@gmail.com>
2026-06-29 22:35:52 +0200

    Correction D3

--- fragment/Dices.gd
+++ fragment/Dices.gd
@@ -10,10 +10,6 @@ static func d6(count: int, threshold:int) -> int :
 			result = result+1
 	return result
 
-static func d3(count: int=1, threshold:int=1) -> int :
-	var result = 0
-	for i in range(0,count) :
-		if _random.randi_range(1,3) >= threshold :
-			result = result+1
-	return result
+static func d3() -> int :
+	return _random.randi_range(1,3)
 	
