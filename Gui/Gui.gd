54f89ae974fd3eb32b0fa37101d7c13ce9b51f4d
Kepka Ludovic <shionn@gmail.com>
2026-06-29 23:15:49 +0200

    Correction poiunter

--- Gui/Gui.gd
+++ Gui/Gui.gd
@@ -1,10 +1,10 @@
 extends Control
 class_name Gui
 
-@onready var cursor = load("res://assets/kenney/cursor-pack/pointer_c.png")
-@onready var point_hand = load("res://assets/kenney/cursor-pack/hand_point.png")
-@onready var drag = load("res://assets/kenney/cursor-pack/hand_closed.png")
-@onready var can_drop = load("res://assets/kenney/cursor-pack/hand_open.png")
+@onready var cursor = load("res://Gui/Assets/kenney/cursor-pack/pointer_c.png")
+@onready var point_hand = load("res://Gui/Assets/kenney/cursor-pack/hand_point.png")
+@onready var drag = load("res://Gui/Assets/kenney/cursor-pack/hand_closed.png")
+@onready var can_drop = load("res://Gui/Assets/kenney/cursor-pack/hand_open.png")
 
 func _ready() -> void:
 	Input.warp_mouse(get_viewport().get_visible_rect().size/2)
