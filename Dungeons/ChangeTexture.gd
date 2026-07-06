b552530a8a4c2e3443683a9a0fb40b4dd998bcca
Kepka Ludovic <shionn@gmail.com>
2026-07-06 17:47:20 +0200

    Change Texture ok mais transpire vers les autres scene

--- Dungeons/ChangeTexture.gd
+++ Dungeons/ChangeTexture.gd
@@ -1,12 +1,21 @@
 extends Node3D
 
+@export var _enable : bool = true
+@export var _texture : Texture2D
+
 func _ready() -> void:
-	for child in self.get_children() :
+	if _enable : apply_to_node(self)
+
+func apply_to_node(elem :Node3D) -> void :
+	for child in elem.get_children() :
 		if child is MeshInstance3D :
 			var mesh = child as MeshInstance3D
 			var i = 0
 			var mat = mesh.get_active_material(i) as StandardMaterial3D
 			while mat != null :
 				print(mat.albedo_texture)
+				mat.albedo_texture = _texture
 				i = i+1
 				mat = mesh.get_active_material(i) as StandardMaterial3D
+		elif child is Node3D :
+			apply_to_node(child)
