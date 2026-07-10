84ecbec090727105bca1b4b276d58f60bc9dd549
Kepka Ludovic <shionn@gmail.com>
2026-07-10 22:37:02 +0200

    Nom pnj

--- Game/PNJ.gd
+++ Game/PNJ.gd
@@ -3,12 +3,18 @@ class_name PNJ
 extends GameBaseCharacterBody3D
 
 @onready var _animation = $Character/AnimationPlayer as AnimationPlayer
+@onready var _name_label = $Label3D as Label3D
 
 @export var interact_distance = 3.0
 
 func _ready() -> void:
 	_animation.play("RigMedium/Idle_A")
+	_name_label.text = name
 	
+	
+func _physics_process(_delta: float) -> void:
+	_name_label.look_at(player.global_position, Vector3.UP, true)
+
 func interact() -> void:
 	print("interact is not overwrite")
 
