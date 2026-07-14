b9a70af5c1c38802d5587dcdb48c4352f3bdf8a5
Kepka Ludovic <shionn@gmail.com>
2026-07-15 00:01:33 +0200

    Auberge

--- Dungeons/00/AlaricDuclair.gd
+++ Dungeons/00/AlaricDuclair.gd
@@ -2,4 +2,5 @@ extends PNJ
 
 func interact() -> void:
 	look_at_player()
+	$"Présentation/Next/Valthorion ?".enable = tags.have(Tags.AUBERGE_PLAYER_KNOW_VALTHORION)
 	gui.openDialog($"Présentation")
