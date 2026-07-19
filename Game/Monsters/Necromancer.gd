cb43fde3afedfda88a31f10ff22371d5235d4323
Kepka Ludovic <shionn@gmail.com>
2026-07-19 11:09:01 +0200

    Necromancer

---
+++ Game/Monsters/Necromancer.gd
@@ -0,0 +1,13 @@
+extends Monster
+
+enum AttackMode { CHOP_2H }
+
+func get_fov() -> float : return 80
+func get_rig() -> String : return "RigMedium/"
+func get_def() -> int : return 2
+func get_hit_sound() -> AudioStreamPlayer3D : return $ZombieYell9
+func get_max_pv() -> int : return 5 
+func is_large() -> bool : return false
+func get_global_colddown() -> float : return 2
+func list_atks() -> Array[MonsterAtk] :  return [$Chop2H]
+func get_min_atk_range() -> int : return 3
