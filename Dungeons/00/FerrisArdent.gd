42ce619e21ab18bcd973936ce0e931b84e7cac4a
Kepka Ludovic <shionn@gmail.com>
2026-07-28 07:00:16 +0200

    Ajout d'un pnj à l'auberge

--- Dungeons/00/FerrisArdent.gd
+++ Dungeons/00/FerrisArdent.gd
@@ -6,3 +6,10 @@ func _ready() -> void:
 	$Character/Rig_Medium/Skeleton3D/RightHand/engineer_Wrench2.queue_free()
 	$Character/Rig_Medium/Skeleton3D/Engineer_Goggles.queue_free()
 	super._ready()
+
+func interact() -> void:
+	look_at_player()
+	if quest_book.auberge_01_reprendre_des_forces.is_done() :
+		gui.openDialog($Dialog)
+	else :
+		gui.openDialog($"DialogueJoueurFatigué")
