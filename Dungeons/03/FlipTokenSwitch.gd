ffe232cff786f185574e4de3cfb1b17bbf34c9d5
Kepka Ludovic <shionn@gmail.com>
2026-08-14 09:20:48 +0200

    Dungeon 03

---
+++ Dungeons/03/FlipTokenSwitch.gd
@@ -0,0 +1,21 @@
+class_name FlipTokenSwitch
+extends Interactable
+
+@onready var black = $checker_black_single
+@onready var white = $checker_white_single
+@export var state: bool = false
+
+signal state_change(token: FlipTokenSwitch)
+
+func _ready() -> void:
+	white.visible = state
+	black.visible = not state
+
+func toggle() -> void : 
+	state = not state
+	white.visible = state
+	black.visible = not state
+
+func _on_activate() -> void:
+	toggle()
+	state_change.emit(self)
---
+++ Dungeons/03/WallTokenFlip.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+
+func _on_token_switch_state_change(token: FlipTokenSwitch) -> void:
+	var x = token.name.substr(0,1).to_int()
+	var y = token.name.substr(1,1).to_int()
+	if x > 1 : _toggle(x-1, y)
+	if x < 5 : _toggle(x+1, y)
+	if y > 1 : _toggle(x, y-1)
+	if y < 5 : _toggle(x, y+1)
+	
+	if all_white() or all_black() :
+		pass
+	
+
+func _toggle(x:int, y:int) -> void:
+	get_node("%d%d"%[x,y]).toggle()
+	
+func all_white() -> bool :
+	for child in get_children() :
+		if child is FlipTokenSwitch and not child.state : return false
+	return true
+
+func all_black() -> bool :
+	for child in get_children() :
+		if child is FlipTokenSwitch and child.state : return false
+	return true
